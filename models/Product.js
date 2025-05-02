1625cc89f743e30d10916424d627f11201b48a68
bm2699 <bm2699@nyu.edu>
2025-05-02 03:54:10 -0400

    Updated shop layout, added product images

---
+++ models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    image: String,
+    dimension: String
+
+});
+
+module.exports = mongoose.model('Product', productSchema);
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+
+//Routes
+const productRoutes = require('./routes/productRoutes');
+app.use('/api/products', productRoutes);
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});  
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`)
+);
+})
+
+.catch((err) => console.error('MongoDB connection error:', err));
